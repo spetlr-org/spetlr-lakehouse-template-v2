a505da874ff3591dea8b19bc0278af7d6a088f92
FarBo <farzadb58@gmail.com>
2024-02-10 21:23:24 +0100

    deploy storage account

--- .github/terraform/main.tf
+++ .github/terraform/main.tf
@@ -7,3 +7,17 @@ resource "azurerm_resource_group" "rg" {
     service = var.service_tag
   }
 }
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = local.resource_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    creator = var.creator_tag
+    system = var.system_tag
+    service = var.service_tag
+  }
+}
