c01e46b71a2ad482dc2cf284d8450fe405d0f476
FarBo <farzadb58@gmail.com>
2024-03-30 20:12:25 +0100

    fix python wheel build

--- .github/deploy/application/build.ps1
+++ .github/deploy/application/build.ps1
@@ -15,7 +15,13 @@ $repoRoot = (git rev-parse --show-toplevel)
 
 Push-Location -Path $repoRoot
 
+# Build Dependencies
+Write-Host "Now Installing Build Dependencies"
+python -m pip install --upgrade pip
+pip install -r requirements-deploy.txt
+
 # we want a really clean build (even locally)
+Write-Host "Clean up before build"
 if (Test-Path -Path dist) {
     Remove-Item -Force -Recurse dist
 }
@@ -28,11 +34,14 @@ if (Test-Path -Path src\*.egg-info) {
 
 pyclean -v .
 
+# Set up environment
+Write-Host "Setting up build environment"
 . "$repoRoot/tools/set_lib_env.ps1" `
     -buildId $buildId `
     -environmentName $environmentName
 
 # Update the spetlr version of the python library
+Write-Host "Updating spetlr version"
 $newVersion = $buildId
 
 $setupCfgContent = Get-Content setup.cfg
@@ -41,6 +50,7 @@ $newSetupCfgContent = $setupCfgContent -replace "(version = ).*", "version = ${n
 Set-Content -Path setup.cfg -Value $newSetupCfgContent
 
 # Build the wheel
+Write-Host "Building the wheel"
 python setup.py bdist_wheel
 
 
