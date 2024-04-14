1cb755217dbb5b8ed0f4a06da7ea9760a958c241
FarBo <farzadb58@gmail.com>
2024-04-14 11:47:39 +0200

    fix

--- .github/deploy/databricks_workspace/databricks_access_control.tf
+++ .github/deploy/databricks_workspace/databricks_access_control.tf
@@ -3,10 +3,10 @@ resource "databricks_external_location" "ex_data_catalog_location" {
   provider = databricks.workspace
   name = "${var.environment}-${var.data_catalog_container}"
   url = "abfss://${var.data_catalog_container}@${local.resource_name}.dfs.core.windows.net/"
-  credential_name = data.ex_storage_cred.display_name
+  credential_name = data.databricks_storage_credential.ex_storage_cred.id
   comment         = "Databricks external location for data catalog"
   depends_on = [
-    data.ex_storage_cred
+    data.databricks_storage_credential.ex_storage_cred
   ]
 }
 
@@ -14,7 +14,7 @@ resource "databricks_grants" "ex_data_catalog_grants" {
   provider = databricks.workspace
   external_location = databricks_external_location.ex_data_catalog_location.id
   grant {
-    principal  = databricks_group.db_ws_admin_group.display_name
+    principal  = databricks_group.admins.display_name
     privileges = ["ALL_PRIVILEGES"]
   }
   depends_on = [databricks_external_location.ex_data_catalog_location]
