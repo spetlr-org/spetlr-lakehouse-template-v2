be233fd1e837d9f14e53e0eb0a307e5e1ae2eedf
FarBo <farzadb58@gmail.com>
2024-04-21 09:32:15 +0200

    fixes

--- .github/deploy/databricks_workspace/upload_workspace_notebooks.tf
+++ .github/deploy/databricks_workspace/upload_workspace_notebooks.tf
@@ -1,4 +1,4 @@
-# This module is for uploading all notebooks reside in the workspace folder to the databricks workspace
+# This module is for uploading all notebooks reside in the workspace folder to databricks workspace /Shared/dataplatform
 
 # First, we want to exctract all directories nested in the workspace folder
 locals {
@@ -20,8 +20,14 @@ locals {
   # Map each directory to its corresponding path in the Databricks workspace
   databricks_directories = { for d in local.unique_directories : d => "${local.db_workspace_base_path}/${d}" }
 
+  # List all files within the 'workspace' directory
+  all_files = fileset(local.base_directory, "**/*")
+
+  # Filter out files from the list of paths
+  files = [for file in local.all_files : file if contains(file, ".")]
+
   # Create a map of file paths to their corresponding workspace destination paths
-  files_to_workspace_paths = { for file in local.files : file => "${local.db_workspace_base_path}/${dirname(file)}" }
+  file_workspace_map = { for file in local.files : file => "${local.db_workspace_base_path}/${file}" }
 }
 
 # Create a Databricks directory for each unique directory found in the workspace
@@ -34,7 +40,7 @@ resource "databricks_directory" "workspace_directories" {
 
 # Sync files to the created Databricks workspace
 resource "databricks_workspace_file" "sync_file" {
-  for_each = local.files_to_workspace_paths
+  for_each = local.file_workspace_map
 
   source = "${local.base_directory}/${each.key}"
   path   = each.value
