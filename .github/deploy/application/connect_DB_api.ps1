7dfde7cf041752c47070b23dab09c6d83747b0b9
FarBo <farzadb58@gmail.com>
2024-06-09 08:02:05 +0200

    try this

--- .github/deploy/application/connect_DB_api.ps1
+++ .github/deploy/application/connect_DB_api.ps1
@@ -15,7 +15,7 @@ $repoRoot = (git rev-parse --show-toplevel)
 ###############################################################################################
 Write-Host "Get Databricks workspace admin SPN credentials" -ForegroundColor Green
 
-$workspaceUrl = Get-KeyVaultSecret -key $workspaceUrlKeyName -keyVaultName $resourceName
+$workspaceUrlFull = Get-KeyVaultSecret -key $workspaceUrlKeyName -keyVaultName $resourceName
 $workspaceClientId = Get-KeyVaultSecret -key $workspaceSpnClientIdKeyName -keyVaultName $resourceName
 $workspaceClientSecret = Get-KeyVaultSecret -key $workspaceSpnClientSecretKeyName -keyVaultName $resourceName
 $tenantId = Get-KeyVaultSecret -key $azureTenantId -keyVaultName $resourceName
@@ -26,6 +26,17 @@ $resourceId = az resource show `
     --resource-type "Microsoft.Databricks/workspaces" `
     --query id `
     --out tsv
+
+Throw-WhenError -output $resourceId
+
+$workspaceUrl = az resource show `
+    --resource-group $resourceGroupName `
+    --name $databricksName `
+    --resource-type "Microsoft.Databricks/workspaces" `
+    --query properties.workspaceUrl `
+    --out tsv
+
+Throw-WhenError -output $workspaceUrl
 
 Write-Host "  Add the SPN to the Databricks Workspace as an admin user" -ForegroundColor DarkYellow
 $accessToken = Set-DatabricksSpnAdminUser `
@@ -43,6 +54,6 @@ $token = ConvertTo-DatabricksPersonalAccessToken `
 
 Write-Host "  Generate .databrickscfg" -ForegroundColor DarkYellow
 Set-Content ~/.databrickscfg "[DEFAULT]"
-Add-Content ~/.databrickscfg "host = $workspaceUrl"
+Add-Content ~/.databrickscfg "host = $workspaceUrlFull"
 Add-Content ~/.databrickscfg "token = $token"
 Add-Content ~/.databrickscfg ""
