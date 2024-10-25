c0d658ffe6d95bd037f17a16b6755a212b624f8e
FarBo <farzadb58@gmail.com>
2024-10-25 11:00:56 +0200

    black format

--- mlops_notebooks/nyc_tlc/nyc_automl.py
+++ mlops_notebooks/nyc_tlc/nyc_automl.py
@@ -19,7 +19,8 @@ from databricks import automl
 from datetime import datetime
 import mlflow
 from mlflow import MlflowClient
-mlflow.set_registry_uri('databricks-uc')
+
+mlflow.set_registry_uri("databricks-uc")
 
 
 # COMMAND ----------
@@ -84,7 +85,9 @@ display(df_features)
 
 # COMMAND ----------
 
-df_features.write.mode("overwrite").option("overwriteSchema", "true").saveAsTable(f"{schema_name}.{feature_table}")
+df_features.write.mode("overwrite").option("overwriteSchema", "true").saveAsTable(
+    f"{schema_name}.{feature_table}"
+)
 
 # COMMAND ----------
 
@@ -97,24 +100,25 @@ df_features.write.mode("overwrite").option("overwriteSchema", "true").saveAsTabl
 train_ratio, val_ratio, test_ratio = 0.7, 0.2, 0.1
 df_features = (
     df_features.withColumn("random", F.rand(seed=42))
-        .withColumn("Split",
-            F.when(F.col("random") < train_ratio, "train")
-            .when(F.col("random") < train_ratio + val_ratio, "validate")
-            .otherwise("test")
-        )
-        .drop("random")
+    .withColumn(
+        "Split",
+        F.when(F.col("random") < train_ratio, "train")
+        .when(F.col("random") < train_ratio + val_ratio, "validate")
+        .otherwise("test"),
+    )
+    .drop("random")
 )
 
 # COMMAND ----------
 
 automl_run = automl.regress(
-    experiment_name = xp_name,
-    experiment_dir = xp_path,
-    dataset = df_features,
-    target_col = "TotalAmount",
-    split_col = "Split", #This required DBRML 15.3+
-    timeout_minutes = 5,
-    exclude_cols ='Id'
+    experiment_name=xp_name,
+    experiment_dir=xp_path,
+    dataset=df_features,
+    target_col="TotalAmount",
+    split_col="Split",  # This required DBRML 15.3+
+    timeout_minutes=5,
+    exclude_cols="Id",
 )
 
 # COMMAND ----------
@@ -124,22 +128,27 @@ automl_run = automl.regress(
 
 # COMMAND ----------
 
-experiment_id = mlflow.search_experiments(filter_string=f"name LIKE '{xp_path}%'", order_by=["last_update_time DESC"])[0].experiment_id
+experiment_id = mlflow.search_experiments(
+    filter_string=f"name LIKE '{xp_path}%'", order_by=["last_update_time DESC"]
+)[0].experiment_id
 
 best_model = mlflow.search_runs(
     experiment_ids=experiment_id,
     order_by=["metrics.test_r2_score DESC"],
     max_results=1,
-    filter_string="status = 'FINISHED'" #filter on mlops_best_run to always use the notebook 02 to have a more predictable demo
+    filter_string="status = 'FINISHED'",  # filter on mlops_best_run to always use the notebook 02 to have a more predictable demo
 )
 best_model
 
 # COMMAND ----------
 
-run_id = best_model.iloc[0]['run_id']
+run_id = best_model.iloc[0]["run_id"]
 
 # Register best model from experiments run to MLflow model registry
-model_details = mlflow.register_model(f"dbfs:/databricks/mlflow-tracking/{experiment_id}/{run_id}/artifacts/model", model_name)
+model_details = mlflow.register_model(
+    f"dbfs:/databricks/mlflow-tracking/{experiment_id}/{run_id}/artifacts/model",
+    model_name,
+)
 
 # COMMAND ----------
 
@@ -154,14 +163,12 @@ client.update_registered_model(
 # COMMAND ----------
 
 # Provide more details on this specific model version
-r2_score = best_model['metrics.test_r2_score'].values[0]
-run_name = best_model['tags.mlflow.runName'].values[0]
+r2_score = best_model["metrics.test_r2_score"].values[0]
+run_name = best_model["tags.mlflow.runName"].values[0]
 version_desc = f"This model version has an R2 test metric of {round(r2_score,4)*100}%. Follow the link to its training run for more details."
 
 client.update_model_version(
-    name=model_details.name,
-    version=model_details.version,
-    description=version_desc
+    name=model_details.name, version=model_details.version, description=version_desc
 )
 
 # We can also tag the model version with the F1 score for visibility
@@ -169,16 +176,14 @@ client.set_model_version_tag(
     name=model_details.name,
     version=model_details.version,
     key="r2_score",
-    value=f"{round(r2_score,4)}"
+    value=f"{round(r2_score,4)}",
 )
 
 # COMMAND ----------
 
 # Set this version as the Challenger model, using its model alias
 client.set_registered_model_alias(
-    name=model_name,
-    alias="Challenger",
-    version=model_details.version
+    name=model_name, alias="Challenger", version=model_details.version
 )
 
 # COMMAND ----------
@@ -189,10 +194,10 @@ client.set_registered_model_alias(
 # COMMAND ----------
 
 try:
-    #Compare the challenger r2 score to the existing champion if it exists
+    # Compare the challenger r2 score to the existing champion if it exists
     champion_model = client.get_model_version_by_alias(model_name, "Champion")
-    champion_r2 = mlflow.get_run(champion_model.run_id).data.metrics['test_r2_score']
-    print(f'Champion r2 score: {champion_r2}. Challenger f1 score: {r2_score}.')
+    champion_r2 = mlflow.get_run(champion_model.run_id).data.metrics["test_r2_score"]
+    print(f"Champion r2 score: {champion_r2}. Challenger f1 score: {r2_score}.")
     metric_f1_passed = r2_score >= champion_r2
 except:
     print(f"No Champion found. Accept the model as it's the first one.")
@@ -200,12 +205,10 @@ except:
 
 # COMMAND ----------
 
-print('register model as Champion!')
+print("register model as Champion!")
 if metric_f1_passed:
     client.set_registered_model_alias(
-        name=model_name,
-        alias="Champion",
-        version=model_details.version
+        name=model_name, alias="Champion", version=model_details.version
     )
 
 # COMMAND ----------
@@ -215,10 +218,15 @@ if metric_f1_passed:
 
 # COMMAND ----------
 
-champion_model = mlflow.pyfunc.spark_udf(spark, model_uri=f"models:/{model_name}@Champion")
+champion_model = mlflow.pyfunc.spark_udf(
+    spark, model_uri=f"models:/{model_name}@Champion"
+)
 
 # COMMAND ----------
 
-df_preds = df_features.withColumn('predictions', champion_model(*champion_model.metadata.get_input_schema().input_names()))
+df_preds = df_features.withColumn(
+    "predictions",
+    champion_model(*champion_model.metadata.get_input_schema().input_names()),
+)
 
 display(df_preds)
